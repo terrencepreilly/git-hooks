9c0ea739e43cb53b7357a34a177ddd7383e1d170
treilly <treilly@savantgroup.com>
2017-11-29 20:44:33 -0500

    Add git hook to ensure that Changelog updated.
    This git hook will make sure that the changelog will have been updated.
    Just copy as follows:
    
        cp CheckChangelog ~/my-repo/.git/hooks/pre-commit
    
    The changelog should be called CHANGELOG.md.  If you want to commit
    without updating the changelog at all, then just do
    
        git commit --no-verify

---
+++ CheckChangelog.hs
@@ -0,0 +1,49 @@
+{- A Git pre-commit hook for ensuring that Changelog has been updated. -}
+
+import System.Process
+import System.IO
+import Data.List
+
+gitCommand :: String
+gitCommand = "git --no-pager diff --cached --name-only"
+
+{- Get a list of staged files. -}
+getStagedChanges :: Handle -> IO ([String])
+getStagedChanges stdout' = do
+    contents <- hGetContents stdout'
+    let lcontents = lines contents
+    return lcontents
+
+_printStagedChanges :: [String] -> IO ()
+_printStagedChanges changes = do
+    if null changes then do
+        return ()
+    else do
+        putStrLn (head changes)
+        _printStagedChanges (tail changes)
+
+changelogInStaged :: [String] -> Bool
+changelogInStaged [] = False
+changelogInStaged (x:xs) =
+    if isInfixOf "CHANGELOG.md" x
+    then True
+    else changelogInStaged xs
+
+_cleanUp :: Handle -> Handle -> Handle -> IO ()
+_cleanUp stdin' stdout' stderr' = do
+    hClose stdin'
+    hClose stdout'
+    hClose stderr'
+    return ()
+
+main :: IO ()
+main = do
+    (stdin', stdout', stderr', _) <- runInteractiveCommand gitCommand
+    changes <- getStagedChanges stdout'
+    let changeLogDefined = changelogInStaged changes
+    if not changeLogDefined then do
+        _cleanUp stdin' stdout' stderr'
+        error "Changelog not staged!"
+    else do
+        _cleanUp stdin' stdout' stderr'
+        return()
